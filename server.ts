f6b50b42b6457b881e08804a45cefa4c5535d55e
MachineKe <markarapsoi72@gmail.com>
2024-05-06 00:04:29 +0300

    Add files via upload

---
+++ server.ts
@@ -0,0 +1,8 @@
+const app = require("./app")
+require("dotenv").config()
+
+// create server
+app.listen(process.env.PORT, () => {
+  console.log(`Server is connected with port ${process.env.PORT}`)
+})
+
