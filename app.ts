078d73666e1d2bef1c4b4cf8b3fe826584556d6c
MachineKe <markarapsoi72@gmail.com>
2024-05-09 16:45:05 +0300

    add cookieparser

--- app.ts
+++ app.ts
@@ -1,4 +1,8 @@
+const cors = require('cors');
+const cookieParser = require("cookieparser");
 const express = require("express") 
 const app = express();
-
 module.exports = app;
+
+
+app.use(express.json({limit: "50mb"}))
